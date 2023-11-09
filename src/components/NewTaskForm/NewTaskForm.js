6f998d075820fefaea3336781c1fe517ac36abbf
Murat <renja.dizz@gmail.com>
2023-11-09 18:32:50 +0600

    add state to input

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -1,9 +1,20 @@
+import React from "react";
 import './NewTaskForm.css';
 
 
-const NewTaskForm = () => {
-    return (<input className="new-todo" placeholder="What needs to be done?" autoFocus/>);
+export default class NewTaskForm extends React.Component {
+    state = {
+        value: ''
+    }
+    onChange = (e) => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <form><input className="new-todo" value={this.state.value} placeholder="What needs to be done?" autoFocus onChange={this.onChange}/>
+            </form>);
+    }
 };
-
-
-export default NewTaskForm;
